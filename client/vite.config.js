c8dc2376f5d16d2f03d671f841fffe93e59286a3
Yashwanth E S <yashwanthes25@gmail.com>
2025-02-21 20:59:18 +0530

    all set 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases limit to 1000kB (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves large dependencies into a separate file
+          }
+        }
+      }
+    }
+  }
+});
