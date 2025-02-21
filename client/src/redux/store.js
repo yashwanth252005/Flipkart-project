1e893439c5eed27c4793a4ec0475359fb2931a0c
Yashwanth E S <yashwanthes25@gmail.com>
2025-02-21 20:33:15 +0530

    corrected store.js and redux@5.0.1

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,19 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { thunk } from "redux-thunk";
 import { getProductsReducer } from "./redusers/productReducer";
 import { getProductDetailReducer } from "./redusers/productReducer";
 import { cartReducer } from "./redusers/CartReducer";
 
+// Combine all reducers
 const reducer = combineReducers({
     getProducts: getProductsReducer,
     getProductDetails: getProductDetailReducer,
     cart: cartReducer,
 });
 
+// Middleware setup
 const middleware = [thunk];
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
+// Use built-in Redux DevTools function (fallback to normal compose if not available)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Create Redux store
+const store = createStore(reducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
